b21fba7a737668b0d85cef8ad285da43c5b65664
eduardoboca <eduardo.boca@gmail.com>
2024-02-12 23:03:12 -0300

    Versão final

--- Imc/Program.cs
+++ Imc/Program.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Imc;
+using IMC;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddLocalStorageServices();
-
+builder.Services.AddLocalStorageServices();//Local Storeage
 await builder.Build().RunAsync();
